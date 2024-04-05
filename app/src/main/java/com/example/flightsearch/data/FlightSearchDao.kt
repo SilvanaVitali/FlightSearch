55d22563d716c1ef115c91e44ea186ae57180301
SilvanaV <silgivia@hotmail.com>
2024-04-05 20:11:27 -0300

    Create a data access object

---
+++ app/src/main/java/com/example/flightsearch/data/FlightSearchDao.kt
@@ -0,0 +1,30 @@
+package com.example.flightsearch.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface FlightSearchDao {
+
+    @Query("SELECT * FROM airport ORDER BY iata_code ASC")
+    fun getAllAirports(): Flow<List<Airport>>
+
+    @Query("SELECT * FROM favorite")
+    fun getAllFavorites(): Flow<List<Favorite>>
+
+    @Query("SELECT * FROM airport WHERE iata_code LIKE '%:searchQuery%' OR name LIKE '%:searchQuery%'")
+    fun getAirportBySearch(searchQuery: String): Flow<List<Airport>>
+
+    @Query("SELECT * FROM airport WHERE iata_code != ':iataCode'")
+    fun getPossibleFlights(iataCode: String): Flow<List<Airport>>
+
+    @Query("SELECT * FROM airport WHERE iata_code = ':iataCode'")
+    fun getAirportByCode(iataCode: String): Flow<Airport>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favorite: List<Favorite>)
+}
